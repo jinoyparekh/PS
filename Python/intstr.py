2b6db602554b18414bb674a1cdcf9a5ec49ce262
Jinoy Parekh <jinoyparekh@users.noreply.github.com>
2020-10-11 21:41:30 -0500

    Update intstr.py

--- Python/intstr.py
+++ Python/intstr.py
@@ -1,1 +1,8 @@
+#This is simple program that returns int as str
 
+print("This program returns int as str")
+num = int(input("Please enter num: "))
+
+for i in range(1, num+1):
+    print(i, end='', sep='')
+    
